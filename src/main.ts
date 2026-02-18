22faa00711d052795e2396c4e3d014add010e0b8
ikandelaki <iraklikandelaki2@gmail.com>
2026-02-18 18:55:51 +0400

    Change background color from light blue to dark gray

--- src/main.ts
+++ src/main.ts
@@ -97,7 +97,7 @@ export default app;
 
 (async () => {
   // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
+  await app.init({ background: "#212529", resizeTo: window });
 
   document.getElementById("pixi-container")!.appendChild(app.canvas);
 
